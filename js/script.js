a504efc1b601b2507f15c08f63216aa6e79a7265
christivan <chrcyber08@gmail.com>
2024-10-22 22:48:53 +0700

    22 okto malam

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-function 
+function submitName() {
+    const newName = document.getElementById('name-input').value.trim();
+    const errorMessage = document.getElementById('error-message');
+    if (newName === "") {
+        errorMessage.textContent = "Nama tidak boleh kosong!";
+    } else {
+        const userNameElement = document.getElementById('user-name');
+        userNameElement.textContent = newName;
+        errorMessage.textContent = "";
+    }
+}
