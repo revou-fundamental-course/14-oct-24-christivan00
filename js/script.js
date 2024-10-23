c25a25c473bf13afe8f248170560162f9d6ef9dc
christivan <chrcyber08@gmail.com>
2024-10-24 00:02:54 +0700

    done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,73 @@
-function submitName() {
-    const newName = document.getElementById('name-input').value.trim();
-    const errorMessage = document.getElementById('error-message');
-    if (newName === "") {
-        errorMessage.textContent = "Nama tidak boleh kosong!";
-    } else {
-        const userNameElement = document.getElementById('user-name');
-        userNameElement.textContent = newName;
-        errorMessage.textContent = "";
-    }
+
+document.getElementById('messageForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const gender = document.querySelector('input[name="gender"]:checked') ? document.querySelector('input[name="gender"]:checked').value : 'Tidak ditentukan';
+    const message = document.getElementById('message').value;
+    document.getElementById('output').innerHTML = `
+        <h3>Output Pesan:</h3>
+        <p><strong>Nama:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Jenis Kelamin:</strong> ${gender}</p>
+        <p><strong>Pesan:</strong> ${message}</p>
+    `;
+});
+
+
+let indexSlideA = 0;
+let indexSlideB = 0;
+
+// Fungsi untuk mengganti slide untuk kelas "photo-b"
+function nextSlideB(n) {
+    showSlideB(indexSlideB += n);
 }
+
+// Fungsi untuk mengganti slide untuk kelas "photo-a"
+function nextSlideA(n) {
+    showSlideA(indexSlideA += n);
+}
+
+// Fungsi untuk menampilkan gambar slide untuk kelas "photo-b"
+function showSlideB(n) {
+    let listimgB = document.getElementsByClassName('photo-b');
+
+    if (n > listimgB.length - 1) {
+        indexSlideB = 0;
+    }
+
+    if (n < 0) {
+        indexSlideB = listimgB.length - 1;
+    }
+
+    for (let i = 0; i < listimgB.length; i++) {
+        listimgB[i].style.display = 'none';
+    }
+
+    listimgB[indexSlideB].style.display = 'block';
+}
+
+function showSlideA(n) {
+    let listimgA = document.getElementsByClassName('photo-a');
+
+    if (n > listimgA.length - 1) {
+        indexSlideA = 0;
+    }
+
+    if (n < 0) {
+        indexSlideA = listimgA.length - 1;
+    }
+
+    for (let i = 0; i < listimgA.length; i++) {
+        listimgA[i].style.display = 'none';
+    }
+
+    listimgA[indexSlideA].style.display = 'block';
+}
+
+setInterval(() => nextSlideB(1), 2500);  
+setInterval(() => nextSlideA(1), 2500); 
+
+
+showSlideB(indexSlideB);
+showSlideA(indexSlideA);
